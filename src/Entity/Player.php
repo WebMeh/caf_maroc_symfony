b84e2ae2f5e6eabac165ea7cdf1c8a0e1e697c3f
Mehdi Rachid <rachidelmahdy2@gmail.com>
2025-02-04 18:06:51 +0100

    Ajout des entités Team et Player avec relations OneToMany et ManyToOne

---
+++ src/Entity/Player.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlayerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlayerRepository::class)]
+class Player
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $position = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $club = null;
+
+    #[ORM\ManyToOne(inversedBy: 'players')]
+    private ?Team $team = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPosition(): ?string
+    {
+        return $this->position;
+    }
+
+    public function setPosition(string $position): static
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    public function getClub(): ?string
+    {
+        return $this->club;
+    }
+
+    public function setClub(string $club): static
+    {
+        $this->club = $club;
+
+        return $this;
+    }
+
+    public function getTeam(): ?Team
+    {
+        return $this->team;
+    }
+
+    public function setTeam(?Team $team): static
+    {
+        $this->team = $team;
+
+        return $this;
+    }
+}
